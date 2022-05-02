989a0d15a4b523de63436ada09f2a1195144211b
İrem Kurt <iremkurt19@gmail.com>
2022-05-02 23:07:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    return drivers.filter(
+        (driver) => driver.toUpperCase() === name.toUpperCase()
+        );
+    }
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter(
+        (driver) => driver.toUpperCase().indexOf(name.toUpperCase()) === 0
+    );
+}
+
+
+
+function matchName(drivers,name){
+    return drivers.filter(
+        (driver) => driver.name.toUpperCase() === name.toUpperCase()
+    );
+
+}
